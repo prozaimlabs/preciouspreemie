a26c6e5b886d2fd0f79fabc5822c555399b33f67
Zubair Abubakar <zuby52@gmail.com>
2023-03-25 14:18:06 +0000

    PREEM-18 added initial error handling

---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { RequestValidationError } from '../errors/request-validation-errors';
+
+const router = express.Router();
+
+router.post('/api/users/signup', [body('email').isEmail().withMessage('Email address must be valid'), body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters')], async (request: Request, response: Response) => {
+    const errors = validationResult(request);
+
+    if (!errors.isEmpty()) {
+        throw new RequestValidationError(errors.array());
+    }
+
+    const { email, password } = request.body;
+
+    console.log('Creating a user ...');
+    throw new DatabaseConnectionError();
+
+    response.send({});
+});
+
+export { router as signupRouter };
