83af566caf312edab40be99dd0204a728f9c8d6b
Zubair Abubakar <zuby52@gmail.com>
2023-03-28 22:42:48 +0100

    PREEM 17 fixed sign up payload

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,9 +8,17 @@ import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true,
+    })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -24,6 +32,10 @@ app.all('*', async (request, response) => {
 app.use(errorHandler);
 
 const start = async () => {
+    if (!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY must be defined');
+    }
+
     try {
         await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
         console.log('connected to mongoDb');
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { BadRequestError } from '../errors/bad-request-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
 import { RequestValidationError } from '../errors/request-validation-errors';
 import { User } from '../models/user';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -24,6 +24,20 @@ router.post('/api/users/signup', [body('email').isEmail().withMessage('Email add
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+        {
+            id: user.id,
+            email: user.email,
+        },
+        process.env.JWT_KEY!
+    );
+
+    // store it on session object
+    request.session = {
+        jwt: userJwt,
+    };
 
     response.status(201).send(user);
 });
