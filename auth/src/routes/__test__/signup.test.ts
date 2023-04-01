79ded8352ce3e70dbbf92f3ff430d0b6b96591bf
Zubair Abubakar <zuby52@gmail.com>
2023-04-01 22:26:04 +0100

    added automated tests for signup endpoint

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,56 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns a 201 on successful signup', async () => {
+    return request(app)
+        .post('/api/users/signup')
+        .send({ email: 'test@test.com', password: 'password' })
+        .expect(201);
+});
+
+it('returns a 400 with an invalid email address', async () => {
+    return request(app)
+        .post('/api/users/signup')
+        .send({ email: 'tetestcom', password: 'password' })
+        .expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+    return request(app)
+        .post('/api/users/signup')
+        .send({ email: 'tetestcom', password: 'p' })
+        .expect(400);
+});
+
+it('returns a 400 with missing email address or password', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({ email: 'test@test.com' })
+        .expect(400);
+
+    await request(app)
+        .post('/api/users/signup')
+        .send({ password: 'asdasdads' })
+        .expect(400);
+}, 5000);
+
+it('disallows duplicate email addresses', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({ email: 'test@test.com', password: 'password' })
+        .expect(201);
+
+    await request(app)
+        .post('/api/users/signup')
+        .send({ email: 'test@test.com', password: 'password' })
+        .expect(400);
+}, 5000);
+
+it('sets a cookie after successful signup', async () => {
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({ email: 'test@test.com', password: 'password' })
+        .expect(201);
+
+    expect(response.get('Set-Cookie')).toBeDefined();
+}, 5000);
