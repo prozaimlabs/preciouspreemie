9c0a1cc3cbf3e0675b74f466530d22a5df6ede17
Zubair Abubakar <zuby52@gmail.com>
2023-12-01 09:12:55 +0100

    [PREEMIE-4] added orders model

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { requireAuth, validateRequest } from '@prozaimlabs/common';
 import { body } from 'express-validator';
 import { natsWrapper } from '../nats-wrapper';
+import mongoose from 'mongoose';
 
 const router = express.Router();
 
@@ -9,10 +10,11 @@ router.post(
     '/api/orders',
     requireAuth,
     [
-        body('name').not().isEmpty().withMessage('Product name is required'),
-        body('price')
-            .isFloat({ gt: 0 })
-            .withMessage('Price must be greater than 0'),
+        body('productId')
+            .not()
+            .isEmpty()
+            .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+            .withMessage('Product id must be provided'),
     ],
     validateRequest,
     async (request: Request, response: Response) => {
